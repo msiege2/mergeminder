a83edca525e3e005b37a6e8a89f872aab0c40bca
Matt Siegel <matt.siegel@onedatascan.com>
2020-01-27 08:18:25 -0500

    Issue #58 - Enabled ConfigurationProperties scanning along with Spring Boot update

--- src/main/java/com/mcs/mergeminder/MergeMinderApplication.java
+++ src/main/java/com/mcs/mergeminder/MergeMinderApplication.java
@@ -2,11 +2,13 @@ package com.mcs.mergeminder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = {"com.mcs.mergeminder"})
+@ConfigurationPropertiesScan
 @EnableAsync
 public class MergeMinderApplication {
 

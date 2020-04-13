3925130cac0a8e976054fc41df09fde64c15c256
Matt Siegel <matt.siegel@onedatascan.com>
2020-04-13 14:10:44 -0400

    #73 Add View projects command via slack

--- src/main/java/com/mcs/mergeminder/dao/MergeRequestRepository.java
+++ src/main/java/com/mcs/mergeminder/dao/MergeRequestRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.mcs.mergeminder.dto.MergeRequestModel;
 
 public interface MergeRequestRepository extends CrudRepository<MergeRequestModel, Integer> {
-	// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+	// This will be AUTO IMPLEMENTED by Spring into a Bean called MergeRequestRepository
 	// CRUD refers Create, Read, Update, Delete
 
 	MergeRequestModel findFirstByProjectAndMrId(String project, Integer mrId);
--- src/main/java/com/mcs/mergeminder/dao/MinderProjectsRepository.java
+++ src/main/java/com/mcs/mergeminder/dao/MinderProjectsRepository.java
@@ -1,10 +1,14 @@
 package com.mcs.mergeminder.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.mcs.mergeminder.dto.MinderProjectsModel;
 
 public interface MinderProjectsRepository extends CrudRepository<MinderProjectsModel, Integer> {
-	// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-	// CRUD refers Create, Read, Update, Delete
+
+	List<MinderProjectsModel> findAllByOrderByNamespaceAscProjectAsc();
+
+	List<MinderProjectsModel> findByNamespaceOrderByProject(String namespace);
 }
